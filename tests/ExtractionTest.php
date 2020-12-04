95faa2747eefa35f4998b526543e104f672dd7aa
Xander Peuscher <xander@label305.com>
2020-12-04 11:41:50 +0100

    Add support for original styles when creating new html for shared string (#1)

--- tests/ExtractionTest.php
+++ tests/ExtractionTest.php
@@ -6,6 +6,7 @@ use Label305\XlsxExtractor\Decorated\DecoratedTextExtractor;
 use Label305\XlsxExtractor\Decorated\DecoratedTextInjector;
 use Label305\XlsxExtractor\Decorated\SharedString;
 use Label305\XlsxExtractor\Decorated\SharedStringPart;
+use Label305\XlsxExtractor\Decorated\Style;
 
 class ExtractionTest extends TestCase {
 
@@ -69,7 +70,7 @@ class ExtractionTest extends TestCase {
         $this->assertEquals(" and ", $mapping[7][2]->text);
         $this->assertEquals("colored", $mapping[7][3]->text);
         $this->assertEquals(" text", $mapping[7][4]->text);
-        $this->assertEquals('This is another description with parially <strong>bold</strong> and <strong>colored</strong> text', $mapping[7]->toHTML());
+        $this->assertEquals('This is another description with parially <strong>bold</strong> and <strong>colored</strong><font> text</font>', $mapping[7]->toHTML());
 
         $mapping[0][0]->text = "Titel voor sheet";
         $mapping[7][0]->text = "Dit is een andere omschrijving met deels ";
@@ -105,13 +106,15 @@ class ExtractionTest extends TestCase {
         $sharedString[] = new SharedStringPart('italic' , false, true);
         $sharedString[] = new SharedStringPart(' and ');
         $sharedString[] = new SharedStringPart('underline' , false, false, true);
+        $sharedString[] = new SharedStringPart(' and ');
+        $sharedString[] = new SharedStringPart('colored text' , false, false, false, new Style());
 
-        $this->assertEquals('This is a test with <strong>bold</strong> and <em>italic</em> and <u>underline</u>', $sharedString->toHTML());
+        $this->assertEquals('This is a test with <strong>bold</strong> and <em>italic</em> and <u>underline</u> and <font>colored text</font>', $sharedString->toHTML());
     }
 
     public function test_sharedString_fillWithHTMLDom()
     {
-        $html = 'This is a test with <strong>bold</strong> and <em>italic</em> and <u>underline</u>';
+        $html = 'This is a test with <strong>bold</strong> and <em>italic</em> and <u>underline</u> and <font>colored text</font>';
         $html = "<html>" . $html . "</html>";
 
         $htmlDom = new DOMDocument;
@@ -129,6 +132,41 @@ class ExtractionTest extends TestCase {
         $this->assertEquals(' and ', $sharedString[4]->text);
         $this->assertEquals('underline', $sharedString[5]->text);
         $this->assertTrue($sharedString[5]->underline);
+        $this->assertEquals(' and ', $sharedString[6]->text);
+        $this->assertEquals('colored text', $sharedString[7]->text);
+    }
+
+    public function test_paragraphWithHTML()
+    {
+        $extractor = new DecoratedTextExtractor();
+        $mapping = $extractor->extractStringsAndCreateMappingFile(__DIR__. '/fixtures/markup.xlsx', __DIR__. '/fixtures/markup-extracted.xlsx');
+
+        $translations = [
+            'Titel voor slide',
+            'Dit is een andere omschrijving met deels <strong>dikgedruk</strong> en <strong>gekleurde</strong> <font>tekst</font>',
+        ];
+
+        foreach ($translations as $key => $translation) {
+            $mapping[$key] = SharedString::paragraphWithHTML($translation, $mapping[$key]);
+        }
+
+        $injector = new DecoratedTextInjector();
+        $injector->injectMappingAndCreateNewFile($mapping, __DIR__. '/fixtures/markup-extracted.xlsx', __DIR__. '/fixtures/markup-injected.xlsx');
+
+        $otherExtractor = new DecoratedTextExtractor();
+        $otherMapping = $otherExtractor->extractStringsAndCreateMappingFile(__DIR__. '/fixtures/markup-injected.xlsx', __DIR__. '/fixtures/markup-injected-extracted.xlsx');
+
+        $this->assertEquals('Titel voor slide', $otherMapping[0][0]->text);
+        $this->assertEquals('Dit is een andere omschrijving met deels ', $otherMapping[1][0]->text);
+        $this->assertEquals('dikgedruk', $otherMapping[1][1]->text);
+        $this->assertEquals(' en ', $otherMapping[1][2]->text);
+        $this->assertEquals('gekleurde', $otherMapping[1][3]->text);
+        $this->assertEquals(' ', $otherMapping[1][4]->text);
+        $this->assertEquals('tekst', $otherMapping[1][5]->text);
+
+        unlink(__DIR__.'/fixtures/markup-extracted.xlsx');
+        unlink(__DIR__.'/fixtures/markup-injected-extracted.xlsx');
+        unlink(__DIR__.'/fixtures/markup-injected.xlsx');
     }
     
 }
